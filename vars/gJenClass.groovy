f0be5d38542b67d91cedcd7f318aada55c03c851
dailycodemode <35090060+dailycodemode@users.noreply.github.com>
2022-04-08 13:06:02 +0100

    Create gJenClass.groovy

---
+++ vars/gJenClass.groovy
@@ -0,0 +1,12 @@
+class gJenClass implements Serializable {
+
+  String branchName
+
+  ProjectBranch(String branchName) {
+    this.branchName = Objects.requireNonNull(branchName)
+  }
+
+  boolean isMaster() {
+    branchName == 'master'
+  }
+}
