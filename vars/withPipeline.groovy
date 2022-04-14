472dfe0c8f83ba41748fedb75da38e221d01b0e7
rmurphy-hmcts <robert.murphy@HMCTS.NET>
2022-04-14 13:02:03 +0100

    up

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -3,7 +3,7 @@ import uk.gov.hmcts.contino.ProjectBranch
 def call(type, String product, String component, Closure body) {
 
   println("withPipeline-call-START")
-  def branch = new ProjectBranch("master") // env.BRANCH_NAME
+  def branch = new ProjectBranch("master") //  env.BRANCH_NAME
 
   def deploymentNamespace = branch.deploymentNamespace()
   def deploymentProduct = deploymentNamespace ? "$deploymentNamespace-$product" : product
