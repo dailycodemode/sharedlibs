2104010277aac2911b8bdd65e3aef4f7f6f1001b
rmurphy-hmcts <robert.murphy@HMCTS.NET>
2022-04-14 12:56:14 +0100

    up

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -5,6 +5,7 @@ def call(type, String product, String component, Closure body) {
   println("withPipeline-call-START")
   println("env.BRANCH_NAME:" + env.BRANCH_NAME)
   println(env.BRANCH_NAME)
+  println(BRANCH_NAME)
   println("branch Name")
   def branch = new ProjectBranch(env.BRANCH_NAME)
 
