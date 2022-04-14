d3dbf0dfb3a8e899b76239334fddccfe908e44ea
rmurphy-hmcts <robert.murphy@HMCTS.NET>
2022-04-14 15:25:42 +0100

    up

--- src/uk/gov/hmcts/contino/CommonPipelineDsl.groovy
+++ src/uk/gov/hmcts/contino/CommonPipelineDsl.groovy
@@ -13,35 +13,43 @@ abstract class CommonPipelineDsl implements Serializable {
   }
 
   void afterCheckout(Closure body) {
+    println("CommonPipelineDsl-afterCheckout")
     after('checkout', body)
   }
 
   void before(String stage, Closure body) {
+    println("CommonPipelineDsl-before")
     callbacks.registerBefore(stage, body)
   }
 
   void after(String stage, Closure body) {
+    println("CommonPipelineDsl-after")
     callbacks.registerAfter(stage, body)
   }
 
   void onStageFailure(Closure body) {
+    println("CommonPipelineDsl-onStageFailure")
     callbacks.registerOnStageFailure(body)
   }
 
   void onFailure(Closure body) {
+    println("CommonPipelineDsl-onFailure")
     callbacks.registerOnFailure(body)
   }
 
   void onSuccess(Closure body) {
+    println("CommonPipelineDsl-onSuccess")
     callbacks.registerOnSuccess(body)
   }
 
   void enableSlackNotifications(String slackChannel) {
+    println("CommonPipelineDsl-enableSlackNotifications")
     config.slackChannel = slackChannel
     steps.env.BUILD_NOTICE_SLACK_CHANNEL = slackChannel
   }
 
   void syncBranchesWithMaster(List<String> branches) {
+    println("CommonPipelineDsl-syncBranchesWithMaster")
     config.branchesToSyncWithMaster = branches
   }
 }
--- src/uk/gov/hmcts/contino/PipelineCallbacksConfig.groovy
+++ src/uk/gov/hmcts/contino/PipelineCallbacksConfig.groovy
@@ -4,22 +4,27 @@ class PipelineCallbacksConfig {
   Map<String, Closure> bodies = new HashMap<>()
 
   void registerBefore(String stage, Closure body) {
+    println("PipelineCallbacksConfig-registerBefore")
     bodies.put('before:' + stage, body)
   }
 
   void registerAfter(String stage, Closure body) {
+    println("PipelineCallbacksConfig-registerAfter")
     bodies.put('after:' + stage, body)
   }
 
   void registerOnStageFailure(Closure body) {
+    println("PipelineCallbacksConfig-registerOnStageFailure")
     bodies.put('onStageFailure', body)
   }
 
   void registerOnFailure(Closure body) {
+    println("PipelineCallbacksConfig-registerOnFailure")
     bodies.put('onFailure', body)
   }
 
   void registerOnSuccess(Closure body) {
+    println("PipelineCallbacksConfig-registerOnSuccess")
     bodies.put('onSuccess', body)
   }
 
